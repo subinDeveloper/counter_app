9f2a6e753728a14477c666fc39970ef52b69091a
subinDeveloper <subisubin322@gmail.com>
2024-05-31 12:24:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react";
 function App() {
+  const [count,setCount] = useState(0)
+
+
+  const increment = () => {
+    count < 10 ? setCount(count + 1) : setCount(count)
+
+  }
+  const decrement = () => {
+    count > 1 ? setCount( count - 1) : setCount(count)
+    
+  }
+  
+  const reset = () =>{
+    setCount(0)
+  }
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="flex bg-black text-gray-300 min-h-screen flex-col gap-12 items-center justify-center">
+     <div className="text-6xl">
+     {count}
+     </div>
+    <div className="flex gap-4 ">
+    <button onClick={increment} className="px-4 py-2 bg-pink-500 text-lg">increment</button>
+        <button onClick={reset} className="px-4 py-2 bg-yellow-400 text-lg">reset</button>
+    <button onClick={decrement} className="px-4 py-2 bg-pink-500 text-lg">decrement</button>
+    </div>
     </div>
   );
 }
